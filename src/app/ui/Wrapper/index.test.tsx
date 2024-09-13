8536beae8635f2bcbd8e1d945f53652abd224c27
Luan Ramos Vicente <luanrv00@gmail.com>
2024-09-13 03:15:29 +0000

    Setup jest

---
+++ src/app/ui/Wrapper/index.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react'
+import React from 'react'
+import {Wrapper} from '.'
+
+describe('Wrapper', () => {
+  it('render Wrapper', () => {
+    render(<Wrapper/>)
+    expect(screen.getByRole('heading')).toBeInTheDocument()
+  })
+})
---
+++ src/app/ui/Wrapper/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Wrapper() {
+  return (
+    <h1>wrapper</h1>
+  )
+}
