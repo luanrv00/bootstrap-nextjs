576db5fdf1a05154c3fc5e77e9cb284836d09f49
Luan Ramos Vicente <luanrv00@gmail.com>
2024-09-13 05:24:52 +0000

    Setup storybook

---
+++ src/app/ui/Wrapper/index.stories.tsx
@@ -0,0 +1,10 @@
+import type {StoryObj, Meta} from '@storybook/react'
+import { Wrapper } from '.'
+
+const meta: Meta<typeof Wrapper> = {
+  component: Wrapper,
+}
+
+export default meta
+
+export const Primary: StoryObj<typeof Wrapper> = {}
