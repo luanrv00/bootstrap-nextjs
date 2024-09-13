6876c8eb8561f7d16166644e3247dde89f26aac0
Luan Ramos Vicente <luanrv00@gmail.com>
2024-09-13 04:30:57 +0000

    Add ci badge on readme [no-ci]

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,8 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('render Wrapper', async ({ page }) => {
   await page.goto('http://localhost:3000');
-
-  // Expect a title "to contain" a substring.
   await expect(page.getByRole('heading')).toHaveText(/wrapper/i);
 });
 
