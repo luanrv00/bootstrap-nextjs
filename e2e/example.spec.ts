df09128e1f1d81660333504aa2b97480ee41fe79
Luan Ramos Vicente <luanrv00@gmail.com>
2024-09-13 05:48:32 +0000

    Fix e2e base url

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('render Wrapper', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('/');
   await expect(page.getByRole('heading')).toHaveText(/wrapper/i);
 });
 
